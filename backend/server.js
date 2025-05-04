ef78e01a29a628023b01f50abd6dffbf767fd7a8
nikunj-kohli <nikunj.kohli.s85@kalvium.community>
2025-05-04 22:21:44 +0530

    login and logout endpoints in postman

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const splitRoutes = require('./routes/splitRoutes'); // MongoDB routes
 const splitRoutesMySQL = require('./routes/splitRoutes.mysql'); // MySQL routes
 const { connectDatabase } = require('./db/database');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+
+
 
 
 const app = express();
@@ -11,6 +15,8 @@ const app = express();
 app.use(express.json());
 app.use('/api', splitRoutes); // MongoDB endpoints
 app.use('/api', splitRoutesMySQL); // MySQL endpoints
+app.use('/api', authRoutes);
+app.use(cookieParser());
 
 connectDatabase();
 const PORT = process.env.PORT || 5000;
